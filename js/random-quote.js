eac3b41d2f4a77eef532065a47ff21ffc6f44405
Mace Dev <bulava.place@gmail.com>
2026-07-05 20:23:41 +0700

    круть

--- js/random-quote.js
+++ js/random-quote.js
@@ -11,53 +11,51 @@
 // 
 
 const QUOTES = [
-    // Me and people i call "friends"
+    // me and people i call "friends"
     "На карман не намажешь", // me
     "Если долго мучаться... то ты устанешь", // me
-    "Меня, бесят, запятые.", // me
-    "📞 Pending...", // me
+    "<i class=\"md-e md-e-telephonereciever\"></i> Pending...", // me
     "Будем знакомы", // me
-    "This is your sign!", // me
+    "This is your sign! <i class=\"md-e md-e-sparkles\"></i>", // me
     "Опа, какие звёзды красивые", // me
-    "Let's be friends!", // me
+    'Let\'s be friends! <i class="md-e md-e-heartsparkling"></i>', // me
     "Старый знакомой Мистеру Интернет", // 4idar
     "Закопирайтил бездарность", // TMK
-    "Закопирайтил бездарность", // lemongd
     "Udalite mne real life", // Pirogi
     "хайпует плесень", // Pirogi
     "ccccccccccccccccccccccc", // LaytyMiron
-    "УРАААААА ТУТ ПАНКИ", // Medal
+    "«Меня, бесят, запятые, в коротких сообщениях", // stysan
     
-    // The internet or cool people
-    "Текст не редактировать, информацию в скобках - удалить.", // the internet
-    "Оплата по ранее указанными реквизитам в течение 15 минут после публикации", // the internet
-    "А помните интернет?", // the internet
-    "эыыы я первый надо нопесать", // the internet
+    // cool people of the internet
+    "Текст не редактировать, информацию в скобках - удалить.", // internet meme
+    "Оплата по ранее указанными реквизитам в течение 15 минут после публикации", // internet meme
+    "Муся, это ты?", // internet meme
+    "А помните интернет?", // internet meme
+    "conputer", // internet meme
+    'я забыб <i class="md-e md-e-sob"></i>', // internet meme
+    "эыыы я первый надо нопесать", // умный комментатор №303
     "Сервера не упали, а легли поспать", // ???
     "Жизнь — это череда событий мешающих сидеть в компьютере", // ???
     "Свобода одного человека заканчивается там, где начинается свобода другого.", // ???
-    "never listen to people, they're dumb", // ???
-    "do things your own way", // ???
+    "Don't listen to people, do things your own way", // ???
     "I am cringe, but I am free.", // ???
+    "По фигне пишите, не занят", // ???
     
-    // Movies & Cartoons
+    // movies & cartoons
     "5 минут — полёт нормальный!", // M&tB
-    "Набираем номерок...", // M&tB
     "Ура, ещё один ремонт!", // M&tB
     "I didn't know you could play the piano", // tADC
     "I only know like two songs", // tADC
     
-    // Books
+    // books
     "2 + 2 = 5", // 1984, George Orwell
     "Обычно ты не глупый. Почему глупый, вопрос?", // Project "Hail Mary"
     "Fist My Bump!", // Project "Hail Mary"
     "Amaze! Amaze! Amaze!", // Project "Hail Mary"
     "Grace Rocky save stars.", // Project "Hail Mary"
     
-    // Games
-    "MANKIND IS DEAD", // ULTRAKILL
-    "BLOOD IS FUEL", // ULTRAKILL
-    "HELL IS FULL", // ULTRAKILL
+    // games
+    "MANKIND IS DEAD. BLOOD IS FUEL. HELL IS FULL.", // ULTRAKILL
     "Big call! Because the panda is big!", // Freedom Planet
     "Bakunawa will rise", // Freedom Planet 2
     "Press on the 7th beat no matter what!", // Rhythm Doctor
@@ -65,14 +63,14 @@ const QUOTES = [
     "I ******* love air-conditioning.", // OMORI
     "Oh, if you could only love!", // Rubato
     
-    // Other
+    // other
     "↑←✓€→®™—ѣ²ѵіѳ′[§°£₽„“³$‰∞”¹‘×©↓ß«", // Ilya Birman
     "Арабская лигатура Бисмиллах Ар-Рахман Ар-Рахим  ﷽", // Unicode Consortium
     "‧₊˚♪𝄞࿐₊˚⊹", // kaomoji
     "Сегодня без цитаты.", // meta-quote
 
-    // Special
-    "С ДНЁМ РОЖДЕНИЯ!!!",
+    // special
+    "С ДНЁМ РОЖДЕНИЯ!!! <i class=\"md-e md-e-excited\"></i>",
 ]
 
 
@@ -94,7 +92,7 @@ function randomFromDate(random_range = 1, date = new Date()) {
             const x = Math.sin(hash + 3) * 43758.5453123;
             var out = Math.floor((x - Math.floor(x)) * random_range)
 
-            console.log(seedStr, hash, x, out)
+            // console.log(seedStr, hash, x, out)
 
             return out;
     }
